1b5e04498af2dd28933f8edb1331e8c241d06ab0
liangchupeng <liangchupeng@yaochufa.com>
2017-05-27 16:01:48 +0800

    示例

--- logout.php
+++ logout.php
@@ -4,5 +4,15 @@ include_once './config.inc.php';
 include_once './uc_client/client.php';
 
 $str = uc_user_synlogout();
-echo $str;die;
-header('Location:index.php');
+echo $str;
+session_destroy();
+
+?>
+
+<script>
+	setTimeout(function () {
+		window.location.href = 'index.php';
+	},1000);
+</script>
+
+//header('Location:index.php');
